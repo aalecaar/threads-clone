0ddca4c02eda54d639d3efd9b7130b287cd85cd6
Alejandro Cardona <alejo.98k@gmail.com>
2024-05-15 18:44:38 -0500

    Add: login view

---
+++ ThreadsClone/Core/Authentication/Views/LoginView.swift
@@ -0,0 +1,86 @@
+//
+//  LoginView.swift
+//  ThreadsClone
+//
+//  Created by xx on 15/05/24.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    @State private var email = ""
+    @State private var password = ""
+    var body: some View {
+        NavigationStack {
+            VStack {
+                Spacer()
+                
+                Image(.threads)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 120, height: 120)
+                    .padding()
+                
+                VStack{
+                    Group {
+                        TextField("Enter your email", text: $email)
+                        
+                        SecureField("Enter your password", text: $password)
+                    }
+                    .font(.subheadline)
+                    .padding(12)
+                    .background(Color(.systemGray6))
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                    .padding(.horizontal, 24)
+                }
+             
+                
+                NavigationLink {
+                    Text("Forgot Password")
+                } label: {
+                    Text("Forgot Password?")
+                        .font(.footnote)
+                        .fontWeight(.semibold)
+                        .padding(.vertical)
+                        .padding(.trailing, 28)
+                        .foregroundStyle(.black)
+                        .frame(maxWidth: .infinity, alignment: .trailing)
+                }
+                
+                Button {
+                    
+                } label: {
+                    Text("Login")
+                        .font(.subheadline)
+                        .fontWeight(.semibold)
+                        .foregroundStyle(.white)
+                        .frame(width: 352, height: 44)
+                        .background(.black)
+                        .clipShape(RoundedRectangle(cornerRadius: 8))
+                }
+                
+                Spacer()
+                
+                Divider()
+                
+                NavigationLink {
+                    Text("Registration View")
+                } label: {
+                    HStack(spacing: 3) {
+                        Text("Don't have an account?")
+                        Text("Sign Up")
+                            .fontWeight(.semibold)
+                    }
+                    .font(.footnote)
+                    .foregroundStyle(.black)
+                }
+                .padding(.vertical, 16)
+                
+            }
+        }
+    }
+}
+
+#Preview {
+    LoginView()
+}
