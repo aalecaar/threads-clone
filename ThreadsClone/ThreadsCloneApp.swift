fd658944231e8f501fc925496d8cfba7d66136e9
Alejandro Cardona <alejo.98k@gmail.com>
2024-05-15 15:21:20 -0500

    Initial Commit

---
+++ ThreadsClone/ThreadsCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsCloneApp.swift
+//  ThreadsClone
+//
+//  Created by xx on 15/05/24.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
